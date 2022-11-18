8d07ced69ccbd02c5cff32d1fe991bb6d3b6ff39
YuliiaKon <u.pabat@gmail.com>
2022-11-19 01:05:04 +0200

    розмітка

---
+++ js/01-gallery.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+// Створення і рендер розмітки на підставі масиву даних galleryItems 
+// і наданого шаблону елемента галереї.
+const galleryContanier = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(items);
+
+galleryContanier.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItems(items) {
+    return items
+        .map(({preview, original, description}) => {
+        return '
+    
+            < div class="gallery__item" >
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a> </div>
+            ';
+    }
+
+).join('')}
