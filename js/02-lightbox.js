f10d06eb353e54e5e942d09e921e4cefa53214dd
YuliiaKon <u.pabat@gmail.com>
2022-11-19 02:41:17 +0200

    pмінила розмітку

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContanier = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryContanier.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryItems(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return `
+                <a class="gallery__link"
+                href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>`;
+        }
+        ).join('');
+}
+
+
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+
+galleryContanier.addEventListener('click', onGalleryContanierClick)
+
+function onGalleryContanierClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. 
+// Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані(.min) файли бібліотеки.
+const instance = basicLightbox.create(`
+<img src="${event.target.dataset.source}" width="800" height="600">`);
+instance.show();
+
+divRef.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+        instance.close();
+    }
+});
+}
+
+
+
+
+
 console.log(galleryItems);
